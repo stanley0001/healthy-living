f139affd829c2601fc58be83a2d0546e31bc12a7
stanley0001 <mungaistanley01@gmail.com>
2023-12-28 18:09:53 +0300

    change curency on order

--- resources/views/backend/order/show.blade.php
+++ resources/views/backend/order/show.blade.php
@@ -29,8 +29,8 @@
             <td>{{$order->first_name}} {{$order->last_name}}</td>
             <td>{{$order->email}}</td>
             <td>{{$order->quantity}}</td>
-            <td>${{$order->shipping->price}}</td>
-            <td>${{number_format($order->total_amount,2)}}</td>
+            <td>Ksh{{$order->shipping->price}}</td>
+            <td>Ksh{{number_format($order->total_amount,2)}}</td>
             <td>
                 @if($order->status=='new')
                   <span class="badge badge-primary">{{$order->status}}</span>
