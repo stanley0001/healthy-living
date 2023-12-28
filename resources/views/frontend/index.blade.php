4d4c8b99ac11cd7a05c7a362d04bf1b06d2df2eb
stanley0001 <mungaistanley01@gmail.com>
2023-12-28 07:26:48 +0300

    change curency to Ksh

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -145,7 +145,7 @@
                                                     $after_discount=($product->price-($product->price*$product->discount)/100);
                                                 @endphp
                                                 <span>${{number_format($after_discount,2)}}</span>
-                                                <del style="padding-left:4%;">${{number_format($product->price,2)}}</del>
+                                                <del style="padding-left:4%;">Ksh{{number_format($product->price,2)}}</del>
                                             </div>
                                         </div>
                                     </div>
@@ -235,7 +235,7 @@
                             <div class="product-content">
                                 <h3><a href="{{route('product-detail',$product->slug)}}">{{$product->title}}</a></h3>
                                 <div class="product-price">
-                                    <span class="old">${{number_format($product->price,2)}}</span>
+                                    <span class="old">Ksh{{number_format($product->price,2)}}</span>
                                     @php
                                     $after_discount=($product->price-($product->price*$product->discount)/100)
                                     @endphp
@@ -447,7 +447,7 @@
                                         @php
                                             $after_discount=($product->price-($product->price*$product->discount)/100);
                                         @endphp
-                                        <h3><small><del class="text-muted">${{number_format($product->price,2)}}</del></small>    ${{number_format($after_discount,2)}}  </h3>
+                                        <h3><small><del class="text-muted">Ksh{{number_format($product->price,2)}}</del></small>    ${{number_format($after_discount,2)}}  </h3>
                                         <div class="quickview-peragraph">
                                             <p>{!! html_entity_decode($product->summary) !!}</p>
                                         </div>
--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    // return $request->user();
+    return "{'message':'Not Authorised'}";
 });
