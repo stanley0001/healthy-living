cc5d81b8a2b9c1e82aa2faeb79bd086d7d2e04db
bernardbrains <bernardmuiruri207@gmail.com>
2023-12-29 04:33:40 +0300

    remove dollar sign

--- resources/views/frontend/pages/checkout.blade.php
+++ resources/views/frontend/pages/checkout.blade.php
@@ -382,7 +382,7 @@
                                             @php
                                                 $total_amount=Helper::totalCartPrice();
                                                 if(session('coupon')){
-                                                    $total_amount=$total_amount-session('coupon')['value'];
+                                                    kshtotal_amount=kshtotal_amount-session('coupon')['value'];
                                                 }
                                             @endphp
                                             @if(session('coupon'))
